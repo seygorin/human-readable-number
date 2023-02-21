8ecc4f5e0a891ec261b8c3a57a6626b302bea590
Sergey Gorin <seygorin@gmail.com>
2023-02-21 18:16:44 +0500

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function toReadable (number) {
-  return
+module.exports = function toReadable(number) {
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (number === 0) {
+    return 'zero';
+  }
+
+  let numToWords = '';
+
+  if (Math.floor(number / 100) > 0) {
+    numToWords += ones[Math.floor(number / 100)] + ' hundred ';
+    number %= 100;
+  }
+
+  if (number >= 10 && number < 20) {
+    numToWords += teens[number - 10];
+    return numToWords.trim();
+  } else if (number >= 20) {
+    numToWords += tens[Math.floor(number / 10)];
+    number %= 10;
+    if (number > 0) {
+      numToWords += ' ' + ones[number];
+    }
+  } else if (number > 0) {
+    numToWords += ones[number];
+  }
+
+  return numToWords.trim();
 }
